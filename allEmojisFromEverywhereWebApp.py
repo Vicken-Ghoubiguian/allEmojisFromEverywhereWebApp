067c236b53348c1668a82f53c572899e3f8d0dd6
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-01-30 10:09:18 +0100

    New route for GitHub emojis...

--- allEmojisFromEverywhereWebApp.py
+++ allEmojisFromEverywhereWebApp.py
@@ -1,13 +1,20 @@
 from flask import Flask
 
+#
 app = Flask(__name__)
 
+#
 @app.route('/')
 def index():
+
+    #
     return 'Web App with Python Flask!'
 
 @app.route('/github')
 def getGitHubEmojis():
+
+    #
     return "GitHub emojis..."
 
+#
 app.run(host='0.0.0.0', port=80)
