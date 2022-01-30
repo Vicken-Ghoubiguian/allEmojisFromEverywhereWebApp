ad4ef9ddfdfb7d3516737c0c1ea08ceb2bef2dd0
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-01-30 10:30:57 +0100

    LopK?KK

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import requests
 
-response = requests.get('https://emojipedia.org/taco/')
+response1 = requests.get('https://emojipedia.org/taco/')
+response2 = requests.get('http://emojipedia.org/man-gesturing-no-light-skin-tone/')
 
-print(response.json())
+print(response1.text)
